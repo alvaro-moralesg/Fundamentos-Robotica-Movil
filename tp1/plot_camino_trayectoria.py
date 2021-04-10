17e6748975209c06efa8db98c98e4c2bb5557c56
Alvaro <alv.moralesg@gmail.com>
2021-04-10 18:22:19 -0300

    Avanca actividades

--- tp1/plot_camino_trayectoria.py
+++ tp1/plot_camino_trayectoria.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import itertools as itt 
 import numpy as np
 
 with open('./log/log.txt','r') as fd:
@@ -25,15 +26,21 @@ ct[1].plot(t,o, color='r', label='Orientacion')
 ct[1].set(title='Trayectoria')
 fig.savefig("./img/camino_trayectoria.png")
 plt.legend()
-plt.show()
 
 ## Marcar puntos
-data = [t, x, y, o]
-tamaño = np.size(data,1)
-divisiones = int(tamaño/9)
-print(tamaño,', ',divisiones) 
-puntos = []
-for i in range(1,10):
-	puntos[i] = [x[divisiones*i] for x in data]
 
-print (puntos)
+#tamaño = np.size(t)
+divisiones = int(np.size(t)/10)
+lst_puntos = []
+for i in range (1, 10):
+	puntos = [t[divisiones*i], x[divisiones*i], y[divisiones*i], o[divisiones*i]]
+	lst_puntos.append(puntos)
+
+cycle_color=['green','blue','red','cyan','magenta','yellow','gray','orange','black']
+
+for i in range (1,10):
+	ct[0].plot(lst_puntos[i-1][1], lst_puntos[i-1][2], marker='.', color = cycle_color[i-1])
+	ct[1].plot(lst_puntos[i-1][0], lst_puntos[i-1][1], marker = '.', color = cycle_color[i-1])
+	ct[1].plot(lst_puntos[i-1][0], lst_puntos[i-1][2], marker = '.', color = cycle_color[i-1])
+	ct[1].plot(lst_puntos[i-1][0], lst_puntos[i-1][3], marker = '.', color = cycle_color[i-1])
+plt.show()
